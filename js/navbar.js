8bacee84609ff2f9d5af1adfa13ae673a47a76b1
WaqasMir <109218668+Muhib-Mirza@users.noreply.github.com>
2022-07-20 21:09:49 +0500

    new design

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+let x = 0;
+
+function toggle() {
+    x++; 
+    console.log("Hello");
+    console.log(x);
+    if(x%2 == 1)
+    {
+        document.querySelector('.nav-mobile-list-container').style.transform = "translate(0,-0.58rem)";
+    }
+    if(x%2 == 0)
+    {
+        document.querySelector('.nav-mobile-list-container').style.transform = "translateX(-11rem)";
+    }
+ }
+
+function remove(){
+    document.querySelector('.nav-mobile-list-container').style.transform = "translateX(-11rem)"
+} 
