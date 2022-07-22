a975e07dbf858acdbe435c21da22afa27c199668
WaqasMir <109218668+Muhib-Mirza@users.noreply.github.com>
2022-07-22 23:16:17 +0500

    body upgradation

--- js/navbar.js
+++ js/navbar.js
@@ -7,13 +7,15 @@ function toggle() {
     if(x%2 == 1)
     {
         document.querySelector('.nav-mobile-list-container').style.transform = "translate(0,-0.58rem)";
+        document.querySelector('.mobile-nav-list').style.transform = "translate(0)";
     }
     if(x%2 == 0)
     {
         document.querySelector('.nav-mobile-list-container').style.transform = "translateX(-11rem)";
+        document.querySelector('.mobile-nav-list').style.transform = "translateX(-11rem)";
     }
  }
 
 function remove(){
-    document.querySelector('.nav-mobile-list-container').style.transform = "translateX(-11rem)"
+    document.querySelector('.nav-mobile-list-container').style.transform = "translateX(-11rem)";
 } 
