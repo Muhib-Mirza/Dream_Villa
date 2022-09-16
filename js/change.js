92e80c8bf6c35ad986b92ce2cf66786d137416fc
WaqasMir <109218668+Muhib-Mirza@users.noreply.github.com>
2022-09-16 20:44:37 +0500

    added

--- js/change.js
+++ js/change.js
@@ -2,8 +2,6 @@ let x = 0;
 
 function toggle() {
   x++;
-  console.log("Hello");
-  console.log(x);
   if (x % 2 == 1) {
     document.querySelector(".nav-mobile-list-container").style.transform =
       "translate(0,-0.58rem)";
